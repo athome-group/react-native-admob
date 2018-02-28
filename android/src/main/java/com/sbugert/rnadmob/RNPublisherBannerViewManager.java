dd075fbc04ef7b905ca1524534b646471fa4d249
Oussama Ghalbzouri <oussama.ghalbzouri@athome.eu>
2018-02-28 11:35:13 +0100

    removed logs

--- android/src/main/java/com/sbugert/rnadmob/RNPublisherBannerViewManager.java
+++ android/src/main/java/com/sbugert/rnadmob/RNPublisherBannerViewManager.java
@@ -169,16 +169,11 @@ class ReactPublisherAdView extends ReactViewGroup implements AppEventListener {
             while (entries.hasNext()) {
                 Map.Entry entry = (Map.Entry) entries.next();
                 if (entry.getValue() instanceof ArrayList) {
-                    Log.v("RNPublisherBannerViewManager", "______________ ArrayList");
-                    adRequestBuilder.addCustomTargeting((String) entry.getKey(), (List<String>)entry.getValue());
-                } else if (entry.getValue() instanceof List) {
-                    Log.v("RNPublisherBannerViewManager", "______________ List");
                     adRequestBuilder.addCustomTargeting((String) entry.getKey(), (List<String>)entry.getValue());
                 } else if (entry.getValue() instanceof String) {
-                    Log.v("RNPublisherBannerViewManager", "______________ String");
                     adRequestBuilder.addCustomTargeting((String) entry.getKey(), (String)entry.getValue());
                 } else {
-                    Log.v("RNPublisherBannerViewManager", entry.getValue() + "Custom Targeting: Type" + entry.getValue().getClass().getName() +  "not supported");
+                    Log.v("RNPublisherBannerViewManager", "Custom Targeting: Type " + entry.getValue().getClass().getName() +  " is not supported");
                 }
             }
         }

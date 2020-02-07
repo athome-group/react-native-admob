4c410ef0f7673a344b6e95518320544dc364ae5b
vandervdb <arnaud.vanderbecq@rea-group.com>
2020-02-07 11:27:06 +0100

    Secure code with try catch

--- RNAdMobInterstitial.js
+++ RNAdMobInterstitial.js
@@ -21,23 +21,28 @@ const _subscriptions = new Map();
 
 const addEventListener = (event, handler) => {
   const mappedEvent = eventMap[event];
-  if (mappedEvent) {
-    let listener;
-    if (event === 'adFailedToLoad') {
-      listener = eventEmitter.addListener(mappedEvent, error => handler(createErrorFromErrorData(error)));
+  try {
+    if (mappedEvent) {
+      let listener;
+      if (event === 'adFailedToLoad') {
+        listener = eventEmitter.addListener(mappedEvent, error => handler(createErrorFromErrorData(error)));
+      } else {
+        listener = eventEmitter.addListener(mappedEvent, handler);
+      }
+      _subscriptions.set(handler, listener);
+      return {
+        remove: () => removeEventListener(event, handler),
+      };
     } else {
-      listener = eventEmitter.addListener(mappedEvent, handler);
+      // eslint-disable-next-line no-console
+      console.warn(`Trying to subscribe to unknown event: "${event}"`);
+      return {
+        remove: () => {
+        },
+      };
     }
-    _subscriptions.set(handler, listener);
-    return {
-      remove: () => removeEventListener(event, handler),
-    };
-  } else {
-    // eslint-disable-next-line no-console
-    console.warn(`Trying to subscribe to unknown event: "${event}"`);
-    return {
-      remove: () => {},
-    };
+  } catch (e) {
+    console.w("An Error has occurred while loading ad", e);
   }
 };
 
